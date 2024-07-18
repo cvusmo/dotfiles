699e2a687932077e19439ee436f4ca450743ec32
cvusmo <blacksheepcosmo@gmail.com>
2024-07-18 01:12:23 -0500

    updating

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,3 +1,12 @@
 if status is-interactive
-    # Commands to run in interactive sessions can go here
+    # Custom Fish prompt
+    function fish_prompt
+        set_color green
+        echo -n (whoami) '@' (hostname) ' '
+        set_color normal
+        echo -n (prompt_pwd)
+        echo -n ' > '
+        set_color normal
+    end
+    set -Ux fish_user_paths $fish_user_paths $HOME/.local/bin
 end
