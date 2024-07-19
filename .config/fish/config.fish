66fa01ec2bfccc8a5e9e03dd5251b448353cf1ff
cvusmo <blacksheepcosmo@gmail.com>
2024-07-19 00:53:39 -0500

    updating fish, alacritty, starship

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,5 +1,20 @@
 if status is-interactive
+<<<<<<< Updated upstream
     # Custom Fish prompt
+=======
+    # set user path
+    set fish_greeting ""
+    echo "hoist the black flag"
+    
+    set -Ux fish_user_paths $fish_user_paths $HOME/.local/bin
+    set -Ux EDITOR nvim
+    set -Ux VISUAL nvim
+
+    fish_vi_key_bindings
+
+    starship init fish | source
+
+>>>>>>> Stashed changes
     function fish_prompt
         set_color green
         echo -n (whoami) '@' (hostname) ' '
