8504a028072fd5ccd1fd169d8d277f85bf8056bb
cvusmo <blacksheepcosmo@gmail.com>
2024-10-10 09:46:54 -0500

    Updated fish config and made functions separate.

--- .config/fish/functions/fish_prompt.fish
+++ .config/fish/functions/fish_prompt.fish
@@ -1,56 +1,42 @@
-function fish_prompt --description 'Write out the prompt'
-    set -l last_status $status
-
-    prompt_login
-
-    echo -n ':'
-
-    # PWD
-    set_color $fish_color_cwd
-    echo -n (prompt_pwd)
+function fish_prompt
+    set_color green
+    echo -n (whoami) '@' (prompt_hostname) ' '
     set_color normal
+    echo -n ' '
 
-    set -q __fish_git_prompt_showdirtystate
-    or set -g __fish_git_prompt_showdirtystate 1
-    set -q __fish_git_prompt_showuntrackedfiles
-    or set -g __fish_git_prompt_showuntrackedfiles 1
-    set -q __fish_git_prompt_showcolorhints
-    or set -g __fish_git_prompt_showcolorhints 1
-    set -q __fish_git_prompt_color_untrackedfiles
-    or set -g __fish_git_prompt_color_untrackedfiles yellow
-    set -q __fish_git_prompt_char_untrackedfiles
-    or set -g __fish_git_prompt_char_untrackedfiles '?'
-    set -q __fish_git_prompt_color_invalidstate
-    or set -g __fish_git_prompt_color_invalidstate red
-    set -q __fish_git_prompt_char_invalidstate
-    or set -g __fish_git_prompt_char_invalidstate '!'
-    set -q __fish_git_prompt_color_dirtystate
-    or set -g __fish_git_prompt_color_dirtystate blue
-    set -q __fish_git_prompt_char_dirtystate
-    or set -g __fish_git_prompt_char_dirtystate '*'
-    set -q __fish_git_prompt_char_stagedstate
-    or set -g __fish_git_prompt_char_stagedstate '✚'
-    set -q __fish_git_prompt_color_cleanstate
-    or set -g __fish_git_prompt_color_cleanstate green
-    set -q __fish_git_prompt_char_cleanstate
-    or set -g __fish_git_prompt_char_cleanstate '✓'
-    set -q __fish_git_prompt_color_stagedstate
-    or set -g __fish_git_prompt_color_stagedstate yellow
-    set -q __fish_git_prompt_color_branch_dirty
-    or set -g __fish_git_prompt_color_branch_dirty red
-    set -q __fish_git_prompt_color_branch_staged
-    or set -g __fish_git_prompt_color_branch_staged yellow
-    set -q __fish_git_prompt_color_branch
-    or set -g __fish_git_prompt_color_branch green
-    set -q __fish_git_prompt_char_stateseparator
-    or set -g __fish_git_prompt_char_stateseparator '⚡'
-    fish_vcs_prompt '|%s'
-    echo
+    # Get the current working directory
+    set -l full_pwd (prompt_pwd)
+    set -l pwd (string replace '~' '🏠' $full_pwd)
 
-    if not test $last_status -eq 0
-        set_color $fish_color_error
+    # Shorten dir path
+    set -l path_parts (string split / $pwd)
+    set -l max_depth 7
+
+    if test (count $path_parts) -gt $max_depth
+        echo -n ' 🏠/'
+        echo -n (string join / (string sub -l $max_depth $path_parts))
+        echo -n '.../ '
+    else
+        echo -n $pwd
     end
 
-    echo -n '➤ '
-    set_color normal
+    # Check if in git repo and display branch
+    if test -d .git
+        set -l branch (git branch --show-current 2>/dev/null)
+        echo -n ' (' $branch ')'
+    end
+        # Mode-specific
+        switch $fish_bind_mode
+          case insert
+            echo -n '   '
+          case default
+            echo -n ' 󰣙 '
+          case normal
+            echo -n ' 󰯉 '
+          case visual
+            echo -n '  '
+        end
+
+        set_color normal
+    end
 end
---
+++ .config/fish/functions/search.fish
@@ -0,0 +1,75 @@
+function search
+
+    sudo cryptsetup isLuks /dev/DRIVE
+    set is_encrypted $status
+
+    if test $is_encrypted -eq 0
+
+        echo -n "Please enter your LUKS password: "
+        read -s password
+        echo
+
+        echo "$password" | sudo -S cryptsetup luksOpen /dev/DRIVE DRIVE_crypt
+        if test $status -ne 0
+            echo "Failed to unlock the drive. Please check your password or device."
+            return 1
+        end
+
+        sudo mkdir -p /mnt/tmp_drive
+        sudo mount /dev/mapper/DRIVE_crypt /mnt/tmp_drive
+        if test $status -ne 0
+            echo "Failed to mount the drive."
+            sudo cryptsetup luksClose DRIVE_crypt
+            return 1
+        end
+    else
+
+        echo "The drive is not encrypted. Mounting it directly."
+        sudo mkdir -p /mnt/tmp_drive
+        sudo mount /dev/DRIVE /mnt/tmp_drive
+        if test $status -ne 0
+            echo "Failed to mount the drive."
+            return 1
+        end
+    end
+
+    # Prompt for the files to search
+    echo -n "Please enter the names of the files you are looking for (separated by space): "
+    read -a file_names 
+
+    set found_files (sudo find /mnt/tmp_drive -type f \( -name "$file_names[1]" -o -name "$file_names[2]" \))
+    
+    if test (count $found_files) -gt 0
+        echo "Files found:"
+        echo $found_files
+    else
+        echo "No files found."
+
+        sudo umount /mnt/tmp_drive
+        if test $is_encrypted -eq 0
+            sudo cryptsetup luksClose DRIVE_crypt
+        end
+        return 0
+    end
+
+    # Prompt for the destination path
+    echo -n "Please enter the destination path to copy the files: "
+    read destination_path
+
+    mkdir -p $destination_path
+
+    for file in $found_files
+        cp "$file" "$destination_path/"
+        if test $status -eq 0
+            echo "Copied: $file to $destination_path/"
+        else
+            echo "Failed to copy: $file"
+        end
+    end
+
+    # Unmount the drive and close it if encrypted
+    sudo umount /mnt/tmp_drive
+    if test $is_encrypted -eq 0
+        sudo cryptsetup luksClose nvme0n1p2_crypt
+    end
+end
