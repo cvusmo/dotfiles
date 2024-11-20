90b5a0cf88cdf71e720d37735595078012acfa90
cvusmo <blacksheepcosmo@gmail.com>
2024-11-20 15:17:08 -0600

    Updated hypr, nvim and waybar

--- .config/nvim/lua/plugins/blackbeard.lua
+++ .config/nvim/lua/plugins/blackbeard.lua
@@ -1,33 +1,11 @@
 -- ~/.config/nvim/lua/plugins/blackbeard.lua
 
 return {
-    -- Install the plugin from GitHub using the correct plugin name
-    "cvusmo/blackbeard-nvim",  -- Directly specify the GitHub repo
+    "cvusmo/blackbeard-nvim",
+    branch = 'dev',  -- Specify dev branch
     config = function()
-        -- Setup the plugin
         require("blackbeard").setup({
-            compile = false,  -- Enable compiling the colorscheme
-            undercurl = true,  -- Enable undercurls
-            commentStyle = { italic = true },
-            functionStyle = {},
-            keywordStyle = { italic = true },
-            statementStyle = { bold = true },
-            typeStyle = {},
-            transparent = false,  -- Do not set background color
-            dimInactive = false,  -- Dim inactive window (`hl-NormalNC`)
-            terminalColors = true,  -- Define `vim.g.terminal_color_{0,17}`
-            colors = {  -- Add/modify theme and palette colors
-                palette = {},
-                theme = { dark = {}, light = {} },
-            },
-            overrides = function(colors)  -- Modify built-in highlights
-                return {}
-            end,
             theme = "dark",  -- Set theme to dark or light
-            background = {  -- Map the value of `background` option to a theme
-                dark = "dark",  -- `vim.o.background = "dark"`
-                light = "light"  -- `vim.o.background = "light"`
-            },
         })
     end,
 }
